7b32f60c58fc3a8969300ee153120806b8f2c3ba
True Coders <114052763+truecoder-001@users.noreply.github.com>
2024-07-13 21:48:42 +0530

    Add files via upload

---
+++ UIProject/MyFirstUi/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyFirstUi
+//
+//  Created by Jaison Joy on 3/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
